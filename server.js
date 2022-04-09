8f70148d56d6ecc6bf5e452a61d7f783534c5ca3
hetianyuanjames <hty1016@ad.unc.edu>
2022-04-08 23:05:04 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,23 +1,20 @@
 import { coinFlips, countFlips, coinFlip, flipACoin } from "./modules/coin.mjs";
-import minimist from 'minimist'; // parses argument options
-import express from 'express'; // minimal & flexible Node.js web application framework
+import minimist from 'minimist'; 
+import express from 'express'; 
 
-// Require Express.js
-// const express = require('express')
-// Strange, this above line is in the instruction, but I need to delete it
-// Maybe ask later in the office hour
+
 const app = express()
 
-var argument = minimist(process.argv.slice(2))
-var name = 'port'
-const HTTP_PORT = argument[name] || 5000
+var min = minimist(process.argv.slice(2))
+var port = 'port'
+const HTTP_PORT = min[port] || 5000
 
-// Create my server
+
 const server = app.listen(HTTP_PORT, () => {
     console.log('App listening on port %PORT%'.replace('%PORT%',HTTP_PORT))
 });
 
-// Check status code endpoint
+
 app.get('/app/', (req, res) => {
     res.statusCode = 200;
     res.statusMessage = 'OK';
@@ -25,7 +22,7 @@ app.get('/app/', (req, res) => {
     res.end(res.statusCode+ ' ' +res.statusMessage)
 });
 
-// Endpoint returning JSON of flip function result
+
 app.get('/app/flip/', (req, res) => {
     res.statusCode = 200;
     let aFlip = coinFlip()
@@ -33,7 +30,7 @@ app.get('/app/flip/', (req, res) => {
     res.writeHead(res.statusCode, {'Content-Type' : 'application/json'});
 })
 
-// Endpoint returning JSON of flip array & summary
+
 app.get('/app/flips/:number', (req, res) => {
     var flips = coinFlips(req.params.number)
     res.status(200).json({"raw" : flips, "summary" : countFlips(flips)})
@@ -53,7 +50,7 @@ app.get('/app/flip/call/tails', (req, res) => {
     res.writeHead(res.statusCode, {'Content-Type': 'text/plain'});
 })
 
-// If not recognized request (other requests)
+
 app.use(function(req, res){
     res.status(404).send('404 NOT FOUND')
 });
